25f2a1a0d91509db97a62c86f1a798ca8734a8c5
Abubakar <gwabstech@gmail.com>
2022-12-14 09:36:30 +0100

    larning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Headings() {
+   
+  return (
+    <h1>This is the Heading Text</h1>
+  )
+}
+
 function App() {
- 
+  return <Headings />;
 }
 
 export default App;
