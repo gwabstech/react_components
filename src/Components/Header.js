c1608aabc5f5c74a26406c5d8cd453f1cc9d6fb0
Abubakar <gwabstech@gmail.com>
2022-12-23 02:28:20 +0100

    stylying

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,22 @@ import React from "react";
 
 function Headings(props) {
     console.log(props);
-    return <h1>This is the heading with the name {props.name} and age {props.age}</h1>;
+    const headingStyle = {
+        padding: "20px",
+        border: "1px solid gray",
+        background: "#fff",
+        margin: "20px 0",
+        backgroundColor: "#FFFF00",
+        
+    
+        
+    }
+    return <div style={headingStyle}>
+    
+        <h1>This is the heading with the name {props.name} and age {props.age}</h1>;
+
+    </div>
+   
 }
 
 export default Headings;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,15 @@
 function Main(props){
-    return <h2>{props.greet}</h2>;
+    const MainStyle = {
+        padding: "20px",
+        border: "1px solid gray",
+        background: "#fff",
+        margin: "20px 0",
+        backgroundColor: "#289C2EB0",
+
+
+
+    }
+    return <h2 style={MainStyle}>{props.greet}</h2>;
 }
 
 export default Main;
