91f2d3567e4ea7b2fc6a7c4aca2158432b71ec8a
Abubakar <gwabstech@gmail.com>
2023-01-23 17:30:21 +0100

    working on style

--- src/Components/Slidebar.js
+++ src/Components/Slidebar.js
@@ -1,4 +1,12 @@
 export default function Slidebar(props){
-    return <h2>Hello This is {props.name}</h2>
+
+    const SliderStyle = {
+        padding: "20px",
+        border: "1px solid gray",
+        background: "#fff",
+        margin: "20px 0",
+        backgroundColor: "#289C92B0",
+    }
+    return <h2 style={SliderStyle}>Hello This is {props.name}</h2>
 }
 
